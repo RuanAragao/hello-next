116b9d5522021b6711b6f4c3bbd7468c02b10201
Ruan Aragão <ruanaragao2@gmail.com>
2019-09-26 13:08:58 -0300

    Navigate between pages

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+const index = () => (
+  <div>
+    <Link href="/about">
+      <a>About page</a>
+    </Link>
+    <p>Hello Next.js</p>
+  </div>
+)
+
+export default index
